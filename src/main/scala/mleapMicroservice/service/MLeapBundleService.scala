7794e1282de8e62e2828900df6fcfaa6921d1a68
astwin <asorianostratio@stratio.com>
2017-06-21 22:49:42 +0200

    mleap service

--- src/main/scala/mleapMicroservice/service/MLeapBundleService.scala
+++ src/main/scala/mleapMicroservice/service/MLeapBundleService.scala
@@ -1,13 +1,36 @@
-package com.stratio.intelligence.mleapMicroservice.service
+import java.io.File
 
-import org.springframework.stereotype.Service
+import ml.combust.bundle.BundleFile
+import ml.combust.bundle.dsl.Bundle
+import ml.combust.mleap.runtime.DefaultLeapFrame
+import ml.combust.mleap.runtime.types._
+import ml.combust.mleap.runtime.MleapSupport._
+import ml.combust.mleap.runtime.transformer.Transformer
+import ml.combust.mleap.runtime.types.StructType
+import resource._
 
-@Service
-class MLeapBundleService {
+import scala.util.{Failure, Success, Try}
 
-  def transformData(data:String):String = {
+class MleapService() {
 
+  private var bundle: Option[Bundle[Transformer]] = None
 
-    "Transformed" + data
+  def setBundle(bundle: Bundle[Transformer]): Unit = synchronized(this.bundle = Some(bundle))
+
+  def unsetBundle(): Unit = synchronized(this.bundle = None)
+
+  def loadModel():Bundle[Transformer]= {
+    (for (bf <- managed(BundleFile(new File("/tmp/model.zip")))) yield {
+      bf.loadMleapBundle()
+    }).tried.flatMap(identity).get
   }
+
+
+  def transform(frame: DefaultLeapFrame): Try[DefaultLeapFrame] = synchronized {
+    bundle.map {
+      _.root.transform(frame)
+    }.getOrElse(Failure(new IllegalStateException("no transformer loaded")))
+  }
+
+
 }
